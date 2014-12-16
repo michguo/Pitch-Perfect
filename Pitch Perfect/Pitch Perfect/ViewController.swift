5ef433c217730f0571b58f4088b4d6c39bf29303
Michelle Guo <shellguo95@gmail.com>
2014-12-16 13:25:16 -0800

    added the microphone button and the stop button, which shows and hides the recording label

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var recordingInProgress: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        recordingInProgress.hidden = false
+        // TODO: Record the user's voice
+        println("in recordAudio")
+    }
 
+    @IBAction func stopRecording(sender: UIButton) {
+        // TODO: Hide the "recordingInProgress" label
+        recordingInProgress.hidden = true
+    }
 }
 
