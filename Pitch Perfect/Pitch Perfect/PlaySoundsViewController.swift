3a99a8fabec57d5caf03858e839547fbd95c5c6a
Michelle Guo <shellguo95@gmail.com>
2014-12-17 00:52:01 -0800

    added stop functionality and set currentTime to be zero each the audio is being played

--- Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -34,7 +34,19 @@ class PlaySoundsViewController: UIViewController {
     @IBAction func playSlow(sender: UIButton) {
         audioPlayer.stop()
         audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0
         audioPlayer.play()
+    }
+    
+    @IBAction func playFast(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 1.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopButton(sender: UIButton) {
+        audioPlayer.stop()
     }
     /*
     // MARK: - Navigation
